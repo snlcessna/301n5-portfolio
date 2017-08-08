c717fa149051d6a33ee7afe3c597f5f6403eaac7
Larry Cessna <larry.cessna@outlook.com>
2017-08-07 17:11:08 -0700

    minor edits 02

--- app.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var Projects (name, describe, projectURL, finishedOn){
-  this.name = name; <!-- Self explanatory -->
-  this.describe = describe; <!-- Description of the project -->
-  this.projectURL = projectURL;
-  this.finishedOn = finishedOn;
-
-}
